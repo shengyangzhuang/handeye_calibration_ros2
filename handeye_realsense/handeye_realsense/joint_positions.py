74f9c40ffd48243381d763f4a47bb67e075d0700
Shengyang Zhuang <shengyangzhuang@outlook.com>
2025-01-23 00:26:55 +0800

    clean the codes

--- handeye_realsense/handeye_realsense/joint_positions.py
+++ handeye_realsense/handeye_realsense/joint_positions.py
@@ -1,37 +1,8 @@
 """
-Author: Shengyang Zhuang
-Date Created: 2024-09-07
-Description: ROS 2 program for joint space control for KUKA LBR iiwa7, script referred to lbr_fri_ros2_stack
-
 Copyright © 2024 Shengyang Zhuang. All rights reserved.
-This script is part of the "Multi-Robot System Prototyping for Cooperative Control in Robot-Assisted Spine Surgery" project and is authored solely by Shengyang Zhuang.
 
-Project Website: https://shengyangzhuang.github.io/mres_thesis/
-Shengyang Zhuang Personal Website: https://shengyangzhuang.github.io/
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at:
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
-# Note: Please retain this header in derivative works.
-
-BibTeX:
-@mastersthesis{zhuang2024multirobot,
-  author    = {Zhuang, Shengyang},
-  title     = {Multi-Robot System Prototyping for Cooperative Control in Robot-Assisted Spine Surgery},
-  school    = {Imperial College London},
-  year      = {2024},
-}
+Contact: https://shengyangzhuang.github.io/
 """
-
-
 import rclpy
 from control_msgs.action import FollowJointTrajectory
 from rclpy.action import ActionClient
